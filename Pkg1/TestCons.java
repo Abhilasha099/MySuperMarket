e382092933b6a321dfd4ff28c05c07312906b737
Abhilasha <shanu.devil9@gmail.com>
2024-09-18 18:17:33 +0530

    Changes done by Abhi after code review

--- Pkg1/TestCons.java
+++ Pkg1/TestCons.java
@@ -6,9 +6,10 @@ public class TestCons {
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String str = "Abhilasha";
+		String str = "Christmas";
 		  String revString = "";
 System.out.println(str.length());
+System.out.println("Changes by Abhilasha for final time");
 		  for (int i=str.length()-1 ; i>=0;i--) 
 		  {
 		   revString+=str.charAt(i);
